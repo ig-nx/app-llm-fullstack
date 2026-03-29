c6eb6b02e0a9adf397e591d8019b8446da6f297a
Ignacio Andrés Leyton Olivares <ignacio.le96@gmail.com>
2026-03-29 17:48:33 -0300

    Change title from 'To Do' to 'Tareas'

--- 002-nextjs-frontend/todo-app/components/layout.js
+++ 002-nextjs-frontend/todo-app/components/layout.js
@@ -3,7 +3,7 @@ import styles from '../styles/layout.module.css'
 export default function Layout(props) {
   return (
     <div className={styles.layout}>
-      <h1 className={styles.title}>To Do</h1>
+      <h1 className={styles.title}>Tareas</h1>
       {props.children}
     </div>
   )
