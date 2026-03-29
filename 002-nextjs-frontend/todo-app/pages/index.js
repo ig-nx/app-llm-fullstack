f09c691a9186643c19d07b47b74a8e96bcbb8739
Ignacio Andrés Leyton Olivares <ignacio.le96@gmail.com>
2026-03-29 17:47:56 -0300

    Update page title for task organizer

--- 002-nextjs-frontend/todo-app/pages/index.js
+++ 002-nextjs-frontend/todo-app/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Full Stack Book To Do</title>
+        <title>Full Stack - Organizador de tareas</title>
         <meta name="description" content="Full Stack Book To Do" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
